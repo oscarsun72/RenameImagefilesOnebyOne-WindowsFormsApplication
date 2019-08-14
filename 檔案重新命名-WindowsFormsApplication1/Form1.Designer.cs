590f184644c99a54b221fa6129279f9c957b57a7
守真 孫 <oscarsun72@hotmail.com>
2019-08-14 22:16:17 +0800

    Add project files.

---
+++ 檔案重新命名-WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace 檔案重新命名_WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 816);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "執行（或直接按「Enter」鍵即可）";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(145, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "方便重新命名檔案";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(24, 749);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(719, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "要處理的資料夾路徑";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(24, 670);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(719, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "重新命名預覽";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox2_DragDrop);
+            this.textBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Blue;
+            this.checkBox1.Location = new System.Drawing.Point(285, 815);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "最上層顯示";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(632, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "按Esc鍵關閉";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(731, 491);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 570);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 27);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click_1);
+            this.comboBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(172, 570);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(144, 27);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(322, 570);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(144, 27);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(481, 570);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(144, 27);
+            this.comboBox4.TabIndex = 10;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(322, 616);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(144, 27);
+            this.comboBox5.TabIndex = 11;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(172, 616);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(144, 27);
+            this.comboBox6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 846);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "檔案重新命名";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
+
---
+++ 檔案重新命名-WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using io=System.IO;
+
+namespace 檔案重新命名_WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //test the pull request!
+            if (io.Directory.Exists( textBox1.Text) && io.Directory.Exists(textBox2.Text))
+            {
+                foreach (var itemF in io.Directory.GetFiles(textBox1.Text))
+                {
+                    io.FileInfo f=new io.FileInfo(itemF);
+                    foreach (var itemT in io.Directory.GetFiles(textBox2.Text))
+                    {
+                        io.FileInfo ft=new io.FileInfo(itemT);
+                        if ( f.LastWriteTime==ft.LastWriteTime && f.Length==ft.Length && f.Name==ft.Name )
+                        {
+
+                            if (io.File.GetAttributes(itemF).ToString().IndexOf( io.FileAttributes.ReadOnly.ToString() )!=-1)
+                            {
+                                io.File.SetAttributes(itemF, io.FileAttributes.Normal);                                
+                            }
+                            io.File.Delete(itemF);
+                            break;
+                        }
+                    }
+                }
+                MessageBox.Show("done!");
+            }
+             else
+	            {
+                    MessageBox.Show("路徑有誤！");
+	            }   
+        }
+
+        
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Text = Clipboard.GetText(); 
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.Text = Clipboard.GetText(); 
+        }
+
+        
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {//https://wijtb.nctu.me/archives/269/
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;//调用DragDrop事件 
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {//https://www.google.com/search?rlz=1C1OKWM_zh-TWTW847TW847&ei=W7wBXbDjG8Kk8AWfkYGoBQ&q=c%23+%E6%8B%96%E6%8B%89%E7%89%A9%E4%BB%B6+%E8%B3%87%E6%96%99%E5%A4%BE&oq=c%23+%E6%8B%96%E6%8B%89%E7%89%A9%E4%BB%B6+%E8%B3%87%E6%96%99&gs_l=psy-ab.3.0.33i160l2.11461.20026..22176...0.0..0.99.232.3......0....1..gws-wiz.......0i30.KqdNNbPjI8A
+            //https://wijtb.nctu.me/archives/269/
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            this.textBox1.Text = filePaths[0];
+        }
+
+        private void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            this.textBox2.Text = filePaths[0];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SimpleList family = new SimpleList();
+
+            // Populate the List            
+            family.Add("沛榮");
+            family.Add("玫儀");
+            family.Add("靖F");
+            family.Add("JT");
+            family.Add("five");
+            family.Add("six");
+            family.Add("seven");
+            family.Add("eight");
+            family.PrintContents();
+            comboBox1.DataSource = family;
+        }
+
+        private void comboBox_Click(ComboBox cmbx)
+        {
+            string x= cmbx.SelectedValue.ToString();
+            if (textBox2.Text== "重新命名預覽")
+            {
+                textBox2.Text = "";
+            }
+            textBox2.Text = textBox2.Text + x;
+
+        }
+
+        class SimpleList : IList //https://docs.microsoft.com/zh-tw/dotnet/api/system.collections.ilist?view=netframework-4.8
+        {
+            private object[] _contents = new object[8];
+            private int _count;
+
+            public SimpleList()
+            {
+                _count = 0;
+            }
+
+            // IList Members
+            public int Add(object value)
+            {
+                if (_count < _contents.Length)
+                {
+                    _contents[_count] = value;
+                    _count++;
+
+                    return (_count - 1);
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            public void Clear()
+            {
+                _count = 0;
+            }
+
+            public bool Contains(object value)
+            {
+                bool inList = false;
+                for (int i = 0; i < Count; i++)
+                {
+                    if (_contents[i] == value)
+                    {
+                        inList = true;
+                        break;
+                    }
+                }
+                return inList;
+            }
+
+            public int IndexOf(object value)
+            {
+                int itemIndex = -1;
+                for (int i = 0; i < Count; i++)
+                {
+                    if (_contents[i] == value)
+                    {
+                        itemIndex = i;
+                        break;
+                    }
+                }
+                return itemIndex;
+            }
+
+            public void Insert(int index, object value)
+            {
+                if ((_count + 1 <= _contents.Length) && (index < Count) && (index >= 0))
+                {
+                    _count++;
+
+                    for (int i = Count - 1; i > index; i--)
+                    {
+                        _contents[i] = _contents[i - 1];
+                    }
+                    _contents[index] = value;
+                }
+            }
+
+            public bool IsFixedSize
+            {
+                get
+                {
+                    return true;
+                }
+            }
+
+            public bool IsReadOnly
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
+            public void Remove(object value)
+            {
+                RemoveAt(IndexOf(value));
+            }
+
+            public void RemoveAt(int index)
+            {
+                if ((index >= 0) && (index < Count))
+                {
+                    for (int i = index; i < Count - 1; i++)
+                    {
+                        _contents[i] = _contents[i + 1];
+                    }
+                    _count--;
+                }
+            }
+
+            public object this[int index]
+            {
+                get
+                {
+                    return _contents[index];
+                }
+                set
+                {
+                    _contents[index] = value;
+                }
+            }
+
+            // ICollection Members
+
+            public void CopyTo(Array array, int index)
+            {
+                int j = index;
+                for (int i = 0; i < Count; i++)
+                {
+                    array.SetValue(_contents[i], j);
+                    j++;
+                }
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return _count;
+                }
+            }
+
+            public bool IsSynchronized
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
+            // Return the current instance since the underlying store is not
+            // publicly available.
+            public object SyncRoot
+            {
+                get
+                {
+                    return this;
+                }
+            }
+
+            // IEnumerable Members
+
+            public IEnumerator GetEnumerator()
+            {
+                // Refer to the IEnumerator documentation for an example of
+                // implementing an enumerator.
+                throw new Exception("The method or operation is not implemented.");
+            }
+
+            public void PrintContents()
+            {
+                Console.WriteLine("List has a capacity of {0} and currently has {1} elements.", _contents.Length, _count);
+                Console.Write("List contents:");
+                for (int i = 0; i < Count; i++)
+                {
+                    Console.Write(" {0}", _contents[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            //comboBox1.DroppedDown=true;
+
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            comboBox_Click(comboBox1);
+        }
+
+        private void comboBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                comboBox_Click(comboBox1);
+
+            }
+
+        }
+
+        private void comboBox1_Click_1(object sender, EventArgs e)
+        {
+            comboBox1.DroppedDown = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+            {
+                return;
+            }
+            if (MessageBox.Show("是否開始資料夾內的檔案重新命名？", "重新命名", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                string path=  textBox1.Text;
+                
+                foreach (string fstr in io.Directory.GetFiles(path))
+                {
+                    io.FileInfo f = new io.FileInfo(fstr);
+                    //if (f.LastAccessTime< DateTime.Today)
+                    //{
+                      switch (f.Extension)
+                        {
+                            case ".jpg":
+                                pictureBox1.ImageLocation = f.FullName;
+                                // pictureBox1.Image = new Bitmap( f.FullName);//二式皆可
+                                return;
+                                //break;
+                            case ".jpeg":                            
+                            case ".png":
+                            case ".bmp":
+                            case ".tif":
+                            case ".tiff":
+                            default:
+                                break;
+                        }
+                    //}
+  
+                }
+            }
+        }
+    }
+}
